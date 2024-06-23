f01620062956c00e884dac8c1f122ebbba5338bd
Kimball Goss <Kimballgoss@gmail.com>
2024-06-23 12:23:02 -0600

    arrow functions completed

--- src/arrow-fns.js
+++ src/arrow-fns.js
@@ -1,12 +1,12 @@
 // Unit 2 Assessment: arrow-fns.js
-
+//THESE HAVE BEEN TESTED ON REPL
 // Set the value of giveMeTwo to an arrow function that returns 2.
 //
 // Ex.:
 //   giveMeTwo();
 //   => 2
 const giveMeTwo = () => {
-    return 2
+    return 2;
 };
 
 // Set the value of addNums to an arrow function that takes in two numbers and
@@ -16,7 +16,7 @@ const giveMeTwo = () => {
 //   addNums(1, 2);
 //   => 3
 const addNums = (numA, numB) => {
-    return numA+numB
+    return numA + numB;
 };
 
 // Set the value of max to an arrow function that takes in two numbers and
@@ -25,14 +25,13 @@ const addNums = (numA, numB) => {
 // Ex.:
 //   max(1, 2);
 //   => 2
-const max = (numbA, numbB); {
-    if(numA > numB){
-        return numA
+const max = (numA, numB) => {
+    if (numA > numB) {
+        return numA;
+    } else {
+        return numB;
     }
-    else{
-        return numB
-    }
-}
+};
 
 // Given an array of numbers, return a new array that only includes the even
 // numbers. Do this using array iteration methods. Do NOT use a for loop.
@@ -41,13 +40,13 @@ const max = (numbA, numbB); {
 //   evens([1, 2, 3, 4, 5]);
 //   => [2, 4]
 function evens(nums) {
-    let newArray = []
+    let newArray = [];
     nums.forEach((element) => {
-        if(element % 2 === 0){
-            newArray.push(element)
+        if (element % 2 === 0) {
+            newArray.push(element);
         }
     });
-    return newArray
+    return newArray;
 }
 
 // Given an array of names, return a new array of greetings for each name. Do
@@ -57,11 +56,11 @@ function evens(nums) {
 //   createGreetings(['Clive', 'Jill', 'Torgal']);
 //   => ['Hello, Clive!', 'Hello, Jill!', 'Hello, Torgal!']
 function createGreetings(names) {
-    let newArray = names
-    newArray.forEach((element) => {
-        element = "Hello, " + element
-    })
-    return newArray
+    let greetingsArray = [];
+    names.forEach((e) => {
+        greetingsArray.push("Hello, " + e + "!");
+    });
+    return greetingsArray;
 }
 
 // Given an array of words, return a new array where each word is uppercased
@@ -72,13 +71,15 @@ function createGreetings(names) {
 //   loudLongWords(['apple', 'pear', 'cake', 'pinata']);
 //   => ['APPLE', 'PINATA']
 function loudLongWords(words) {
-    let newArray = []
+    let newArray = [];
     words.forEach((element) => {
-        if(element.length > 4){
-            newArray.push(element.toUpperCase)
+        //console.log(element)
+        if (element.length > 4) {
+            newArray.push(element.toUpperCase());
         }
-    })
-    return newArray
+    });
+    return newArray;
 }
 
 export { addNums, createGreetings, evens, giveMeTwo, loudLongWords, max };
+
